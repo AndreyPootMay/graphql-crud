797b0be59956b24db824d0e6ddac0ad3bf7d0098
AndreyPootMay <spocker312@gmail.com>
2021-01-24 19:25:26 -0500

    First commit

---
+++ src/tasks.js
@@ -0,0 +1,20 @@
+export const tasks = [
+    {
+        _id: 0,
+        title: 'Do homework',
+        description: 'A long detail section',
+        number: 100,
+    },
+    {
+        _id: 1,
+        title: 'Run',
+        description: 'A long detail section',
+        number: 90,
+    },
+    {
+        _id: 3,
+        title: 'Learn js',
+        description: 'learn js xdd',
+        number: 87,
+    },
+];
